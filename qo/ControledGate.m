51ff442713451edf9e52b5259dd514b63ed34c20
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2003-11-25 15:18:12 +0000

    Large changes in ProductGate and ControledGate

--- qo/ControledGate.m
+++ qo/ControledGate.m
@@ -1,34 +1,54 @@
-#_Not = [0,1;1,0];
-#_H = 1/sqrt(2)*[1,1;1,-1];
+## -*- texinfo -*-
+## @deftypefn ControledGate(@var{size},@var{gate},@var{contrv},@var{targetv} )
+## Function @code{ControledGate} generates operator of size equal 2^@var{size}.
+## Parameter @var{gate} is 2x2 basic operator (ex. Not). Paramters
+## @var{contrv},@var{targetv} are lists of indices of control and target qubits.
+## @end deftypefn
 
-function ret = ControledGate(invek,gate,contrv,targetv)
+## Author: Piotr Gawron, Jaroslaw Miszczak
+## Created: 25 November 2003
+
+function ret = ControledGate(size,gate,contrv,targetv)
+
+if(nargin!=4)
+	usage("ControledGate(size,gate,contrv,targetv)\n");
+endif
+if(!is_scalar(size))
+	error("1st parameter should be scalar");
+endif
+if(is_square(gate)!=2)
+	error("2nd parameter should be matrix 2x2");
+endif
+
+if(size < max(max(contrv),max(targetv)))
+	error("Operator acts on %d qubits, max control index is %d, max target index is %d", size, max(contrv), max(targetv));
+endif
 
 proj1 = [0,0;0,1];
-
 
 sort(contrv);
 sort(targetv);
 
-flags = zeros(1,log2(length(invek)));
+flags = zeros(1,size);
 
 for i = 1:length(contrv)
 	ctmp = contrv(i);
-	flags(ctmp) = 1; 
+	flags(ctmp) = 1;
 endfor
 
 for i = 1:length(targetv)
 	ttmp = targetv(i);
-	if flags(ttmp) != 0 
-		printf("Error: arguments overlaping\n");
+	if flags(ttmp) != 0
+		error("arguments overlaping");
 		return;
-	else		
-	flags(ttmp) = 2; 
+	else
+	flags(ttmp) = 2;
 	endif
 endfor
 
 # initialization
 
-ret = Id(length(invek));
+ret = Id(size);
 
 if (flags(1) == 1)
 	tmp = proj1;
@@ -37,12 +57,12 @@ elseif (flags(1) == 2)
 elseif (flags(1) == 0)
 	tmp = Id(1);
 else
-	printf("Error: unknown error occured\n");
-endif	
+	error("unknown error occured");
+endif
 
 # matrix building
 
-for i = 2:log2(length(invek))
+for i = 2:size
 if (flags(i) == 1)
 	tmp = kron(tmp,proj1);
 elseif (flags(i) == 2)
@@ -50,12 +70,12 @@ elseif (flags(i) == 2)
 elseif (flags(i) == 0)
 	tmp = kron(tmp,Id(1));
 else
-	printf("Error: unknown error occured\n");
+	error("unknown error occured");
 endif
 endfor
 
-tmp = Id(log2(length(invek))) - tmp
+tmp = Id(size) - tmp;
 
-ret = tmp*invek;	
+ret = tmp;
 
 endfunction
--- qo/ProductGate.m
+++ qo/ProductGate.m
@@ -1,26 +1,47 @@
-function ret = ProductGate(gate,dotvek,regsize)
+## -*- texinfo -*-
+## @deftypefn ProductGate(@var{size},@var{gate},@var{targetv} )
+## Function @code{ProductGate} generates operator of size equal 2^@var{size}.
+## Parameter @var{gate} is 2x2 basic operator (ex. Not). Paramter
+## @var{targetv} is list of indices of target qubits.
+## @end deftypefn
+
+## Author: Piotr Gawron, Jaroslaw Miszczak
+## Created: 25 November 2003
+
+function ret = ProductGate(size,gate,targetv)
+if(nargin!=3)
+	usage("ProductGate(size,gate,targetv)");
+endif
+if(!is_scalar(size))
+	error("1st parameter should be scalar");
+endif
+if(is_square(gate)!=2)
+	error("2nd parameter should be matrix 2x2");
+endif
+if(size < max(targetv))
+	error("Operator acts on %d qubits, max target index is %d", size, max(targetv));
+endif
+if (min(targetv)<1)
+	error("Qubit index less than 0");
+endif
+
 idx = 1;
-sort(dotvek);
-if (dotvek(1) == 1)
+sort(targetv);
+if (targetv(1) == 1)
 	tmp = gate;
 	idx++;
 else
 	tmp = Id(1);
 endif
 
-if (max(dotvek)>regsize || min(dotvek)<1)
-	error("ProductGate: qubit index greater then register given size or less than 0");
-	return;
-endif
-
-for i = 2:regsize
-if((idx <= length(dotvek)) && (dotvek(idx) == i))
+for i = 2:size
+if((idx <= length(targetv)) && (targetv(idx) == i))
 	tmp = kron(tmp,gate);
 	idx++;
 else
 	tmp = kron(tmp,Id(1));
-endif	
+endif
 
 endfor
-ret = tmp;	
+ret = tmp;
 endfunction
