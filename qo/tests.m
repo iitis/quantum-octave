a5cc87fd0e4adb0e5e2e5383a399288ca911951e
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2003-12-02 12:40:05 +0000

    Files ket1.m and v1.m deleted. New file test.m added.

--- qo/tests.m
+++ qo/tests.m
@@ -8,3 +8,10 @@ b = ket([1,0]')
 
 ProductGate(a,[0,1;1,0],[1,2])
 ProductGate(a,1/sqrt(2)*[1,1;1,-1],[1,2,4])
+a = [1,2,3]';
+
+if (isvector(a))
+printf ("Mam vector");
+endif
+
+printf("Ala ma kota Shrodingera");
--- qo/v1.m
+++
@@ -1,7 +0,0 @@
-a = [1,2,3]';
-
-if (isvector(a))
-printf ("Mam vector");
-endif
-
-printf("Ala ma kota Shrodingera");
