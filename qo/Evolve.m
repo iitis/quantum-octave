c131f7c00486db5ed53babe500a1bdff26d5bff3
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2003-12-11 11:13:33 +0000

    Some updates

---
+++ qo/Evolve.m
@@ -0,0 +1,22 @@
+## -*- texinfo -*-
+## @deftypefn {Function File} {} Evolve ()
+## For example:
+## @example
+## @group
+## Evolve(ProductGate (2,Not,[1,2]),State (Ket ([0,1])))
+## @result{}
+##	0  0  0  0
+##	0  0  0  0
+##	0  0  1  0
+##	0  0  0  0
+## @end group
+## @end example
+## @end deftypefn
+
+## Author: Piotr Gawron, Jaroslaw Miszczak
+## Created: 25 November 2003
+
+function ret = Evolve(evolution, state)
+
+	ret = evolution*state*conj(evolution);
+endfunction
