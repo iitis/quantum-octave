0094faccaa3842d8106d371d7f6c81645c267b06
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2004-02-24 09:28:17 +0000

    Evolve bug fixed. Deutsch's algorithm added.

--- qo/isunitary.m
+++ qo/isunitary.m
@@ -1,10 +1,8 @@
 function ret = isunitary(matrix)
-#matrix*conj(matrix)
-#norm((matrix*conj(matrix))-eye(size(matrix)),"fro")
 	if(size(matrix)(1)!=size(matrix)(2))
 		ret = 0;
 		return;
-	elseif(norm((matrix*conj(matrix))-eye(size(matrix)),"fro")<1e-15)
+	elseif(norm((matrix*matrix')-eye(size(matrix)),"fro")<1e-15)
 		ret = 1;
 		return;
 	else
