911e3f650aed5f169e6da8a4838ec3025d4955cf
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2004-03-23 12:12:23 +0000

    Usful script;)

---
+++ qo/qo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This file is part of quantum-octave package. It can be redistributed 
+# under terms of GNU General Public License. More information on web 
+# page http://quantum-octave.sourceforge.net/.
+#
+# (c) 2004 Jarek Miszczak
+# Last modyfication: Tue Mar 23 12:19:51 UTC 2004
+
+PWD_PATH=$(dirname $0);
+if [ ${PWD_PATH} == "." ]; then
+	QO_PATH=$(pwd);
+else
+	QO_PATH=${PWD_PATH};
+fi
+export OCTAVE_PATH=$OCTAVE_PATH:${QO_PATH}:${QO_PATH}/examples
+octave
