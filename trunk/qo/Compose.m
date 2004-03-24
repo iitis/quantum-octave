ddddb0e1686daedf7e3f422c2cf4e3ec276eb905
piotr_gawron <piotr_gawron@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2004-03-24 11:57:56 +0000

    ControledGate still buggy :(

---
+++ trunk/qo/Compose.m
@@ -0,0 +1,31 @@
+## -*- texinfo -*-
+## @deftypefn{Function file} {} Name([@var{size}] )
+## Function @code{Id(@var{size})} returns 2^size x 2^size dentity
+## matrix, argument is optional, if not suplied is set to 1.
+## 
+## @example 
+## @group
+## Name(2)
+##  @result{}
+##	1  0  0  0 
+##	0  1  0  0
+##	0  0  1  0
+##	0  0  0  1
+## @end group
+## @end example
+## @end deftypefn
+## @seealso {Id, Not, H, Pase}
+## 
+## Author: Piotr Gawron, Jaroslaw Miszczak
+## Created: 12 December 2003
+
+
+function ret = Compose(varargin)
+
+ret = va_arg();
+nargin--;
+while (nargin--)
+        ret = kron(va_arg(),ret);
+endwhile
+
+endfunction
---
+++ trunk/qo/Decode.m
@@ -0,0 +1,66 @@
+## -*- texinfo -*-
+## @deftypefn{Function file} {} Name([@var{size}] )
+## Function @code{Id(@var{size})} returns 2^size x 2^size dentity
+## matrix, argument is optional, if not suplied is set to 1.
+## 
+## @example 
+## @group
+## Name(2)
+##  @result{}
+##	1  0  0  0 
+##	0  1  0  0
+##	0  0  1  0
+##	0  0  0  1
+## @end group
+## @end example
+## @end deftypefn
+## @seealso {Id, Not, H, Pase}
+## 
+## Author: Piotr Gawron, Jaroslaw Miszczak
+## Created: 23 March 2004
+
+function ret = Decode(state, code)
+
+flag = -1; # 0-bitflip, 1-faseflip, 2-shor
+
+if(nargin!=2)
+        usage("Encode(state, \"biflip\"|\"phaseflip\"|\"shor\")");
+endif
+
+if(strcmp(code,"bitflip"))
+        flag=0;
+elseif(strcmp(code,"phaseflip"))
+        flag=1;
+elseif(strcmp(code,"shor"))
+        flag=2;
+else
+        usage("Encode(state, \"biflip\"|\"phaseflip\"|\"shor\")");
+endif
+
+qs = log2(size(state)(2)); # number of qubits in the input state
+
+if (flag==0) # bitflip
+	if (qs!=3)
+	        error("state should be density matrix of 3 qubit system")
+	endif
+        g = ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]); # encoding gate
+        ret = Evolve(g, state);
+elseif(flag==1) # faseflip
+	if (qs!=3)
+	        error("state should be density matrix of 3 qubit system")
+	endif
+        g = ProductGate(3,H,[1,2,3]);
+        g*= ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]); # encoding gate
+        ret = Evolve(g, state);
+elseif(flag==2) # shor
+ 	if (qs!=9)
+	        error("state should be density matrix of 9 qubit system")
+	endif
+        gc = ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]);
+        g = Kron(gc,gc,gc);
+        g*= ProductGate(9,H,[1,4,7]);
+	g*= ControledGate(9,Not,[1],[4])*ControledGate(9,Not,[1],[7]);
+        ret = Evolve(g, state);
+endif
+
+endfunction
