d9ae6de253c8435fe3baec510ce95b969b49a105
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2004-03-25 13:36:09 +0000

    Changes, changes... I'm allready lost...

--- qo/Encode.m
+++ qo/Encode.m
@@ -45,18 +45,18 @@ if(qs!=1)
 endif
 
 if (flag==0) # bitflip
-	g = ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]); # encoding gate
-	newstate = Compose(state, State(Ket([0,0])));
+	g = ControlledGate(3,Not,[1],[2])*ControlledGate(3,Not,[1],[3]); # encoding gate
+	newstate = Kron(state, State(Ket([0,0])));
 	ret = Evolve(g, newstate);
 elseif(flag==1) # faseflip
-	g = ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]); # encoding gate
+	g = ControlledGate(3,Not,[1],[2])*ControlledGate(3,Not,[1],[3]); # encoding gate
 	g*= ProductGate(3,H,[1,2,3]);
-	newstate = Compose(state, State(Ket([0,0])));
+	newstate = Kron(state, State(Ket([0,0])));
 	ret = Evolve(g, newstate);
 elseif(flag==2) # shor
-	g = ControledGate(9,Not,[1],[4])*ControledGate(9,Not,[1],[7]); 
+	g = ControlledGate(9,Not,[1],[4])*ControlledGate(9,Not,[1],[7]); 
 	g*= ProductGate(9,H,[1,4,7]);
-	gc= ControledGate(3,Not,[1],[2])*ControledGate(3,Not,[1],[3]); 
+	gc= ControlledGate(3,Not,[1],[2])*ControlledGate(3,Not,[1],[3]); 
  	g*= Kron(gc,gc,gc);
 	newstate = Compose(state, State(KetN(0,8)));
 	ret = Evolve(g, newstate);
