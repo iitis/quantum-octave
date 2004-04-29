1ee89685516a62f755b3ee96a5546296b0808818
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2004-04-29 14:05:26 +0000

    TagQubit and TagQubitWithError moved to examples.

--- trunk/qo/tests/tests.m
+++ trunk/qo/tests/tests.m
@@ -1,7 +1,7 @@
 #x = ket([0,1,1]')
 
-a = ket([0,1,0,1]')
-b = ket([1,0]')
+a = Ket([0,1,0,1]')
+b = Ket([1,0]')
 
 #Not(x,[1,2])
 
@@ -11,7 +11,7 @@ ProductGate(a,1/sqrt(2)*[1,1;1,-1],[1,2,4])
 a = [1,2,3]';
 
 if (isvector(a))
-printf ("Mam vector");
+printf ("vector");
 endif
 
 printf("Ala ma kota Shrodingera");
