d947ebafe8b8adbeb00bb3f2e7f7fa2015426b95
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2004-05-17 08:02:42 +0000

    Documentation updated.

--- qo/Commutator.m
+++ qo/Commutator.m
@@ -1,26 +1,27 @@
 ## -*- texinfo -*-
-## @deftypefn {Function file} {} H()
-## Function @code{H} returns 2x2 Hadamard matrix, that is
+## @deftypefn {Function file} {} Commutator (@var{mtx1}, @var{mtx2})
+## For two given matrices this function return their comutator.
 ## 
 ## @example 
 ## @group
-## H()
+## Commutator (Id, H)
 ##  @result{} 
-##	0.70711   0.70711
-##	0.70711  -0.70711
+##	0	0
+##	0	0
 ## @end group
 ## @end example
 ##
 ## @end deftypefn
 ##
-## @seealso {Id, Not}
+## @seealso {Id, H, Not}
 ##
 ## Author: Piotr Gawron, Jaroslaw Miszczak
 ## Created: 25 November 2003
+## Last modyfication: 17 May 2004
 
 function ret = Commutator(A,B)
 if (nargin!=2)
-	error("Function takes two parameters!")
+	usage ("Commutator (mtx_1, mtx_2)");
 endif
 if(size(A)!=size(B))
 	error("Matrices are of different sizes!");
