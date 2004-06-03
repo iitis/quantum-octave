ed280840b0bdfff18b486321da5c2068905957b9
piotr_gawron <piotr_gawron@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2004-06-03 11:44:39 +0000

    ControlledGate now works with many control qubits, Phase now takes 2 parameters.

--- trunk/qo/ControlledGate.m
+++ trunk/qo/ControlledGate.m
@@ -40,44 +40,42 @@ if(gatesize < max(max(contrv),max(targetv)))
 endif
 
 
-if(size(targetv)(2)!=1)
-	error("Sorry this function is not implemented yet :-( !")
-endif
 
+proj0 = [1,0;0,0];
 proj1 = [0,0;0,1];
 
-lc = sort(contrv);
-lt = sort(targetv);
+# sort input
+lc = sort(contrv); # control list
+lt = sort(targetv); # targe list
 
 flags = zeros(1,gatesize);
 
 for i = 1:length(lc)
 	ctmp = lc(i);
-	flags(ctmp) = 1;
+	flags(ctmp) = 1; # control==1
 endfor
 
 for i = 1:length(lt)
 	ttmp = lt(i);
-	if (flags(ttmp) != 0)
+	if (flags(ttmp) == 1)
 		error("Arguments overlaping!");
 		return;
 	else
-		flags(ttmp) = 2;
+		flags(ttmp) = 2;  # target==2
 	endif
 endfor
 
 # initialization
 ret = Id(gatesize);
 
-
-# matrix building
+# matrix building - case |111...>
 tmp = 1;
 
 for i = 1:gatesize
 if (flags(i) == 1)
 	tmp = kron(tmp,proj1);
 elseif (flags(i) == 2)
-	tmp = kron(tmp,Id(1) - gate);
+	tmp = kron(tmp,gate);
 elseif (flags(i) == 0)
 	tmp = kron(tmp,Id(1));
 else
@@ -85,8 +83,38 @@ else
 endif
 endfor
 
-tmp = Id(gatesize) - tmp;
-
 ret = tmp;
+
+#matrix building - other cases
+tmp=1;
+for j = 0:2^(size(lc)(2))-2 # for each control qubit
+#genereate permutation of 0 and 1
+perm = Dec2BinVec(j,size(lc)(2));
+pi=1; # permutation index
+	for i = 1:gatesize
+		if (flags(i) == 1) # it is control 
+			if(perm(pi)==0)
+				pi++;
+				tmp = kron(tmp,proj0);
+			elseif(perm(pi)==1)
+				pi++;
+				tmp = kron(tmp,proj1);
+			else
+				error("Internal error occured, it should never happen :-(!");				
+			endif
+		elseif (flags(i) == 2) # it is target, then put Id
+			tmp = kron(tmp,Id(1));
+		elseif (flags(i) == 0)
+			tmp = kron(tmp,Id(1));
+		else
+			error("Internal error occured, it should never happen :-(!");
+		endif
+	endfor
+ret += tmp;
+tmp=1;
+endfor
+
+
+
 
 endfunction
