9ce3ee1622eb65646f350e9d8bd0984fa38f8a97
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2004-07-08 08:13:18 +0000

    Minor modifications, help fixed.

--- trunk/qo/MeasureZ.m
+++ trunk/qo/MeasureZ.m
@@ -1,9 +1,9 @@
 ## -*- texinfo -*-
-## @deftypefn{Function file} {} Measure(@var{state})
-## Function @code{Measure} performs ortogonal measurment on state @var{state}. Function returns a probability distribution of results. 
+## @deftypefn{Function file} {} MeasureZ(@var{state})
+## Function @code{MeasureZ} performs ortogonal measurment on state @var{state}. Function returns a probability distribution of results. 
 ## @example 
 ## @group
-## Measure(State(Ket([1,0,1])))
+## MeasureZ(State(Ket([1,0,1])))
 ##  @result{}
 ## 0  0
 ## 1  0
@@ -17,28 +17,29 @@
 ## @end group
 ## @end example
 ## @end deftypefn
-## @seealso {Id, Not, H, Pase}
+## @seealso {Measure, Evolve, Id, Not, H, Pase}
 ## 
 ## Author: Piotr Gawron, Jaroslaw Miszczak.
 ## Created: 12 December 2003.
-## Last modyfication: 10 February 2004.
+## Last modyfication: 9 July 2004.
 
 function ret = MeasureZ(state)
-  state_size = size(state)(1);
+if ( nargin != 1 )
+	usage ("MeasureZ (state)");
+endif
+state_size = size(state)(1);
 #  qubit_count = log2(state_size)
   
-  ret = zeros(2,state_size);
-  ret(1,:) = 0:state_size-1;
-  temp = zeros(state_size);
-  
-  temp(1,1)=1;
-  ret(2,1)=trace(temp*state);
+ret = zeros(2,state_size);
+ret(1,:) = 0:state_size-1;
+temp = zeros(state_size);
+temp(1,1)=1;
+ret(2,1)=trace(temp*state);
 
-  for i=2:state_size
-  temp(i-1,i-1)=0;
-  temp(i,i)=1;
-  
-  ret(2,i)=trace(temp*state);
-  endfor
- ret = ret'; 
+for i=2:state_size
+	temp(i-1,i-1)=0;
+	temp(i,i)=1;
+  	ret(2,i)=trace(temp*state);
+endfor
+ret = ret'; 
 endfunction
