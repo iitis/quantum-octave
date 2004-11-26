fa1da1775807df23cbb6f5e9a1b7144448324f67
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2004-11-26 09:11:46 +0000

    Some clean-ups and documentation update.

--- qo/examples/Teleport.m
+++ qo/examples/Teleport.m
@@ -1,45 +1,64 @@
 ## -*- texinfo -*-
 ## @deftypefn{Function file} {} Teleport(@var{state})
-## 
+## The @code{Teleport} function performs teleportation
+## protocol for one qubit density matrix @var{sate}. Try
+##
+## @example
+## @group
+## Teleport(KetN(1,1))
+## @end group
+## @end example
+##
+## @end deftypefn
+##
+## @seealso{TeleportWithError}
+##
 ## Author: Piotr Gawron, Jaroslaw Miszczak
 ## Created: 04 April 2004
+## Last modyfication: 26 November 2004
 
 function ret = Teleport(st)
 
-if(nargin!=1)
+if (nargin != 1)
 	usage("Teleport (state)");
+else 
+	qs = log2(size(st)(2)); # number of qubits in the input state
+	
+	if(qs!=1)
+		error("State should be density matrix of 1 qubit system!");
+	endif
+	
+	# preparation of compound state
+	lstate = kron(st, State(Ket([0,0]))); 
+
+	# preparation of bell state
+	g1 = Circuit(ProductGate(3, H, [2]), ControlledGate(3, Not, [2], [3]));
+	lstate = Evolve(g1, lstate);
+
+	#
+	g2 = Circuit(ControlledGate(3,Not,[1],[2]), ProductGate(3,H,[1]));
+
+	lstate = Evolve(g2, lstate);
+
+	o = Measure(lstate,"ZZI");
+
+	lstate = o.state;
+	outcome = o.eigval;
+
+	# throw away qubits after measurement
+	lstate = PTrace(lstate,[1,2]); 
+
+	# if second qubit is 1 then, negate teleported state
+	if (outcome(2)==-0.5)
+		lstate = Evolve(Not, lstate);
+	endif
+
+	# if first qubit is 1 then, perform Z on teleported state	
+	if (outcome(1)==-0.5) 
+		lstate = Evolve(Sz, lstate);
+	endif
+
+	ret = lstate;
+	printf("Teleportation done.\n");
 endif
-
-qs = log2(size(st)(2)); # number of qubits in the input state
-
-if(qs!=1)
-	error("State should be density matrix of 1 qubit system!")
-endif
-lstate = kron(st, State(Ket([0,0]))); # preparation of bigger state
-
-#preparation of bell state
-g1 = Circuit(ProductGate(3, H,[2]), ControlledGate(3,Not,[2],[3]));
-lstate = Evolve(g1, lstate);
-
-#
-g2 = Circuit(ControlledGate(3,Not,[1],[2]), ProductGate(3,H,[1]));
-
-lstate = Evolve(g2, lstate);
-
-o = Measure(lstate,"ZZI");
-
-lstate = o.state;
-outcome = o.eigval;
-
-lstate = PTrace(lstate,[1,2]); # throw away qubits after measurement
-
-if (outcome(2)==-0.5) # if second qubit is 1 then, negate teleported state
-	lstate = Evolve(Not, lstate);
-endif
-if (outcome(1)==-0.5) # if first qubit is 1 then, perform Z on teleported state
-	lstate = Evolve(Sz, lstate);
-endif
-
-ret = lstate;
-printf("Teleportation done.\n");
 endfunction
