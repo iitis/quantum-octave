c494d6e8b9f4d6bbc2d1f1426e46eeb0654abb8b
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2005-03-09 12:59:27 +0000

    Unwanted files deleted.

--- trunk/qo/tests/b.m
+++
@@ -1,7 +0,0 @@
-function ret = b(x)
-if (x==1/2)
-	ret = [0];
-elseif (x==-1/2)
-	ret = [1];
-endif
-endfunction
--- trunk/qo/tests/prob_ent.m
+++
@@ -1,6 +0,0 @@
-function ret = prob_ent(st, x, y)
-b(x);
-b(x);
-obs = Projection([b(x),b(y)]);
-ret = trace(st*obs);
-endfunction
