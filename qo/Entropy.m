4c993bbac3412698e9e4a1448381055dd2f4b786
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2005-07-14 09:52:57 +0000

    Small change

--- qo/Entropy.m
+++ qo/Entropy.m
@@ -1,6 +1,6 @@
 ## -*- texinfo -*-
 ## @deftypefn {Function file} {} Entropy(s)
-## Function @code{H} calculates entropy of mixed (or pure) state
+##  Function @code{H} calculates entropy of mixed (or pure) state
 ## @var{s} acording to the rule 
 ## @code{Entropy(s)=-sum(eig(s)log2(eig(2))}
 ## 
@@ -10,36 +10,30 @@
 ##  @result{} 
 ##	1	
 ## @end group
-## @group
-## Entropy(MaxMix(2))
-##	@result{}
-##	2	
 ## @end example
-##
 ## @end deftypefn
 ##
 ## @seealso {PTrace, PTranspose, Evolve, Negativity}
-##
-## Author: Piotr Gawron, Jaroslaw Miszczak
-## Created: 22 March 2004
 
 function ret = Entropy(state)
-ret = 0;
-tmp = 0;
-if ( nargin != 1 )
-	usage ("Entropy (state)");
-else	
-	ev = eig(state); # eigenvalues of density matrix
-	nb = size(ev)(1); # number of eigenvalues
-	for  i = 1:nb 
-		if ( ev(i) == 0.0 )
-			ret -= 0;	
-		else
-			ret -= ev(i)*log2(ev(i));
-		endif
-	endfor
-endif	
-if (ret < 10e-15) 
-	ret = 0.0;
-endif	
+	ret = 0;
+	tmp = 0;
+
+	if ( nargin != 1 )
+		usage ("Entropy (state)");
+	else	
+		ev = eig(state); # eigenvalues of density matrix
+		nb = size(ev)(1); # number of eigenvalues
+		for  i = 1:nb 
+			if ( ev(i) == 0.0 )
+				ret -= 0;	
+			else
+				ret -= ev(i)*log2(ev(i));
+			endif
+		endfor
+	endif	
+
+	if (ret < 10e-15) 
+		ret = 0.0;
+	endif	
 endfunction
