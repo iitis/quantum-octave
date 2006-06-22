6116903efe9e0f83d902c307ddd1537237ba0913
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2006-06-22 13:03:29 +0000

    Function 'BuildBinaryVector' renamed to 'BinVec'

--- trunk/qo/BinVec.m
+++ trunk/qo/BinVec.m
@@ -1,14 +1,16 @@
 ## -*- texinfo -*-
-## @deftypefn {Function file} {} BuildBinaryVector(@var{indx},@var{binvec},@var{binnum},@var{qubits})
-## Function @code{BuildBinaryVector} returns a  @code{Ket} base vector  in @var{qubits}-qubits Hilbert 
-## space labeled with number constructed as follows. Every bit of this number which is listed in @var{indx}
-## is sert according with @var{binnum}, other bits are complemented using @var{binnum}.
+## @deftypefn {Function file} {} BinVec(@var{indx},@var{binvec},@var{binnum},@var{qubits})
+## Function @code{BinVec} returns a @code{Ket} base vector in 2^@var{qubits}-dimensional 
+## Hilbert space labeled with number constructed as follows. Every bit 
+## of this number which is listed in @var{indx} is set according with 
+## @var{binnum}, other bits are complemented using @var{binnum}.
+##
 ## Note: @var{binvec} and @var{binnum} must be binary vectors such that 
 ## @code{length of binvec + length of binnum = qubits}.
 ## 
 ## @example 
 ## @group
-## BuildBinaryVector([1,2],[1,0],[1,1],4)
+## BinVec([1,2],[1,0],[1,1],4)
 ##  @result{} 
 ##  0
 ##  0
@@ -34,17 +36,24 @@
 ## @seealso {Id, Not}
 ##
 ## Author: Piotr Gawron, Jaroslaw Miszczak.
-## Created: 8 March 2005,
-## Last modyfication: 16 March 2004.
+## Created: 8 March 2005.
+## Last modyfication: 22 June 2006.
 
-function ret = BuildBinaryVector(qidx, binvec, binnum, qubits)
+function ret = BinVec(qidx, binvec, binnum, qubits)
+
+# number of arguments must be equal to 4
+if ( nargin != 4 )
+	usage("BinVec (qidx,binvec,binnum,qubits)");
+endif
+
+# sort bit substitution list
 indx = sort(qidx);
 
 is = size(indx)(2); # number of bits for substitution
-bs = size(binvec)(2); # number of bits in substuitution list
+bs = size(binvec)(2); # number of bits in substitution list
 ns = size(binnum)(2); #number of bits in complementation
 
-# test validity of call
+# can we make it?
 
 if ( is != bs )
 	error("Size of first (=%d) and second argument (=%d) do not match!",is, bs);
@@ -57,6 +66,8 @@ endif
 if ( indx(is) > qubits )
 	error("Too large qubit index (=%d) in first argument!", indx(is));
 endif
+
+# it seems so...
 
 # local variables for looping over binvec and binnum lists
 qi = 1;
