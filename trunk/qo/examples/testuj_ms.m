f6ec83e985dbea6ba92d171dacbff4957f7a6c94
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-01-26 10:59:06 +0000

    Added nice handling for number of arguments

--- trunk/qo/examples/testuj_ms.m
+++ trunk/qo/examples/testuj_ms.m
@@ -1,25 +1,41 @@
-function ret=testuj_ms(errType,noExp,noTry)
+function ret=testuj_ms(errType,noExp,noRates,type)
 
-	if(nargin!=3)
-		usage("testuj_ms(errType, noExp, noTry)");
+	if(nargin<1)
+		message = [ 
+			"testuj_ms(noRates, [noExp, errGate, type])\n"\
+			"\tnoRates - number of error rates for calculating output\n"\
+			"\tnoExp - number of experiments for each rate\n"\
+			"\terrGate - error used (defualt - dephasing chanell)\n"\
+			"\ttype - 'fid' for calculating fidelity of 'exp' for full game "];
+		usage(message);
 	else
 
-		# number of experiments
+        	# number of experiments
+	        MAX = noExp;
+        	# number of error rates tested
+	        TRY = noRates;
 
-		MAX = noExp;
-		# number of error rates tested
-		TRY = noTry;
-	
-		ret = zeros(TRY,1);
+        	ret = zeros(TRY,1);
 
-		for p = 0:TRY
-			wynik=zeros(MAX,1);
-			for i = 1:MAX 
-				wynik(i)=MagicSquares(3,1, (p/TRY)*errType+(1-p/TRY)*Id(1), [1,2,3,4]); 
-			endfor
-			ret(p+1,1)=p/TRY;
-			ret(p+1,2)=mean(wynik);
-		endfor
+                for p = 0:TRY
+                        seg=(1-p/TRY)*Id(1)+(p/(3*TRY))*(Sx+Sy+Sz);
+                        error_gate=ProductGate(4,seg,[2,3]);
+                        wynik=zeros(MAX,1);
+                        if param=="fid"
+                                wynik=MagicSquaresFidelity(3,1, error_gate);
+                                ret(p+1,1)=p/TRY;
+                                ret(p+1,2)=wynik;
+                        elseif param=="exp"
+                                for i = 1:MAX
+                                        wynik(i)=MagicSquares(3,1, error_gate);
+                                        printf("p=%d, i=%d\n",p,i);
+                                endfor
+                                ret(p+1,1)=p/TRY;
+                                ret(p+1,2)=mean(wynik);
+                        else
+                                error("param != exp or fid");
+                        endif
+                endfor
 	endif
 	
 endfunction
