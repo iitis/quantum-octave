c417cd6c41039bdb7902ff3aa4a1f1e763df962a
Jarek Miszczak <15267034+jmiszczak@users.noreply.github.com>
2007-01-26 08:53:15 +0000

    Adder params to testuj_ms

--- qo/examples/testuj_ms.m
+++ qo/examples/testuj_ms.m
@@ -1,18 +1,25 @@
-function ret=testuj_ms()
-	# number of experiments
-	MAX = 1000;
-	# number of error rates tested
-	TRY = 100;
-	
-	ret = zeros(TRY,1);
+function ret=testuj_ms(errType,noExp,noTry)
 
-	for p = 0:TRY
-		wynik=zeros(MAX,1);
-		for i = 1:MAX 
-			wynik(i)=MagicSquares(3,1, (p/TRY)*Sx+(1-p/TRY)*Id(1), [1,2,3,4]); 
+	if(nargin!=3)
+		usage("testuj_ms(errType, noExp, noTry)");
+	else
+
+		# number of experiments
+
+		MAX = noExp;
+		# number of error rates tested
+		TRY = noTry;
+	
+		ret = zeros(TRY,1);
+
+		for p = 0:TRY
+			wynik=zeros(MAX,1);
+			for i = 1:MAX 
+				wynik(i)=MagicSquares(3,1, (p/TRY)*errType+(1-p/TRY)*Id(1), [1,2,3,4]); 
+			endfor
+			ret(p+1,1)=p/TRY;
+			ret(p+1,2)=mean(wynik);
 		endfor
-		ret(p+1,1)=p/TRY;
-		ret(p+1,2)=mean(wynik);
-	endfor
+	endif
 	
 endfunction
