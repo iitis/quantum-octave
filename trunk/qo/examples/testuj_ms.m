f1301a7a29c9bd0075b6da75b9358bf4e72ae578
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-01-26 14:20:16 +0000

    more improvements

--- trunk/qo/examples/testuj_ms.m
+++ trunk/qo/examples/testuj_ms.m
@@ -1,41 +1,47 @@
-function ret=testuj_ms(errType,noExp,noRates,type)
+function ret=testuj_ms(noExp,noRates,type,errType)
 
 	if(nargin<1)
 		message = [ 
-			"testuj_ms(noRates, [noExp, errGate, type])\n"\
+			"testuj_ms(noExp,noRates,type,errType)\n"\
 			"\tnoRates - number of error rates for calculating output\n"\
 			"\tnoExp - number of experiments for each rate\n"\
-			"\terrGate - error used (defualt - dephasing chanell)\n"\
+			"\terrType - error used (defualt - dephasing chanell)\n"\
 			"\ttype - 'fid' for calculating fidelity of 'exp' for full game "];
 		usage(message);
 	else
+		# number of experiments
+	  MAX = noExp;
+    # number of error rates tested
+	  TRY = noRates;
 
-        	# number of experiments
-	        MAX = noExp;
-        	# number of error rates tested
-	        TRY = noRates;
+		ret = zeros(TRY,1);
+		for p = 0:TRY
+			
+			if (nargin>3) # specific erro type
+				seg=(1-p/TRY)*Id(1)+(p/TRY)*errType;
+			elseif	
+				seg=(1-p/TRY)*Id(1)+(p/(3*TRY))*(Sx+Sy+Sz);
+			endif
+			
+			error_gate=ProductGate(4,seg,[1,4]);
+			
+			wynik=zeros(MAX,1);
+			
+			if type=="fid"
+				wynik=MagicSquaresFidelity(3,1, error_gate);
+				ret(p+1,1)=p/TRY;
+				ret(p+1,2)=wynik;
+			elseif type=="exp"
+				for i = 1:MAX
+					wynik(i)=MagicSquares(3,1, error_gate);
+				endfor
+				ret(p+1,1)=p/TRY;
+				ret(p+1,2)=mean(wynik);
+			else
+				error("param != exp or fid");
+			endif
 
-        	ret = zeros(TRY,1);
-
-                for p = 0:TRY
-                        seg=(1-p/TRY)*Id(1)+(p/(3*TRY))*(Sx+Sy+Sz);
-                        error_gate=ProductGate(4,seg,[2,3]);
-                        wynik=zeros(MAX,1);
-                        if param=="fid"
-                                wynik=MagicSquaresFidelity(3,1, error_gate);
-                                ret(p+1,1)=p/TRY;
-                                ret(p+1,2)=wynik;
-                        elseif param=="exp"
-                                for i = 1:MAX
-                                        wynik(i)=MagicSquares(3,1, error_gate);
-                                        printf("p=%d, i=%d\n",p,i);
-                                endfor
-                                ret(p+1,1)=p/TRY;
-                                ret(p+1,2)=mean(wynik);
-                        else
-                                error("param != exp or fid");
-                        endif
-                endfor
+		endfor
 	endif
-	
+
 endfunction
