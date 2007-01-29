e5ee6a1f1aefa1f995d88a1bd7413d4b5dacf33f
piotr_gawron <piotr_gawron@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-01-29 12:27:09 +0000

    testuj_ms merged

--- trunk/qo/examples/testuj_ms.m
+++ trunk/qo/examples/testuj_ms.m
@@ -1,4 +1,4 @@
-function ret=testuj_ms(noExp,noRates,type,errType)
+function ret=testuj_ms(noExp,noRates,type,errType,errIndex)
 
 	if(nargin<1)
 		message = [ 
@@ -23,8 +23,8 @@ function ret=testuj_ms(noExp,noRates,type,errType)
 				seg=(1-p/TRY)*Id(1)+(p/(3*TRY))*(Sx+Sy+Sz);
 			endif
 			
-			error_gate=ProductGate(4,seg,[1,4]);
 			
+                        error_gate=ProductGate(4,seg,errIndex);
 			wynik=zeros(MAX,1);
 			
 			if type=="fid"
@@ -34,6 +34,8 @@ function ret=testuj_ms(noExp,noRates,type,errType)
 			elseif type=="exp"
 				for i = 1:MAX
 					wynik(i)=MagicSquares(3,1, error_gate);
+					printf("p=%d, i=%d\n",p,i);
+					fflush(1);
 				endfor
 				ret(p+1,1)=p/TRY;
 				ret(p+1,2)=mean(wynik);
@@ -43,5 +45,4 @@ function ret=testuj_ms(noExp,noRates,type,errType)
 
 		endfor
 	endif
-
 endfunction
