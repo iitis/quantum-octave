13d0603275c495bbbe8633a100150b368275961d
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-01-29 14:44:25 +0000

    Help updated

--- trunk/qo/Fidelity.m
+++ trunk/qo/Fidelity.m
@@ -7,32 +7,25 @@
 ##
 ## @seealso {Encode, Decode, Recover, Entropy, TrNorm}
 ##
-## Author: Piotr Gawron, Jaroslaw Miszczak
-## Created: 29 March 2004
 
 function ret = Fidelity(rho,sigma)
 if (nargin!=2)
 	usage("Fidelity (rho, sigma)");
+else 
+	sr = size(rho);
+	lrho = 0;
+	if(2.^ceil(log2(sr))==sr && sr(1)==sr(2)) # it is state
+		lrho = rho;
+	elseif(2.^ceil(log2(sr))==sr && sr(1)==1) # it is state vector
+		lrho = State(rho);
+	else
+		error("First parameter must be state vector or density matrix!")
+	endif
+
+	sqrtrho = sqrtm(lrho);
+	temp = sqrtrho*sigma*sqrtrho;
+	temp = sqrtm(temp);
+	ret = trace(temp);
 endif
-
-sr = size(rho);
-
-lrho = 0;
-
-if(2.^ceil(log2(sr))==sr && sr(1)==sr(2)) # it is state
-	lrho = rho;
-elseif(2.^ceil(log2(sr))==sr && sr(1)==1) # it is state vector
-	lrho = State(rho);
-else
-	error("First parameter must be state vector or density matrix!")
-endif
-
-sqrtrho = sqrtm(lrho);
-
-temp = sqrtrho*sigma*sqrtrho;
-
-temp = sqrtm(temp);
-
-ret = trace(temp);
 
 endfunction
