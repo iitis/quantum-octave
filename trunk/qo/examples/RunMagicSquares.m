70f2ed2ac771a3a98f47e5cb28e1a99deb56bbc8
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-02-26 21:24:10 +0000

    Control scripts for MagicSquares game

---
+++ trunk/qo/examples/RunMagicSquares.m
@@ -0,0 +1,19 @@
+function RunMagicSquares
+
+	measure="prb";
+  	error_name="dep";
+	
+	for ga=[1:1]#3
+		for gb=[1:1]#3
+			out=abs(RunMagicSquaresGame([ga,gb],10,measure(1:3)));
+			name=sprintf("ms_%s_game_%d_%d_error_%s",measure,ga,gb, error_name);
+			printf(name);
+			printf("\n");
+			fflush(1);
+			save ("-text", name, "out");
+			#mesh(out);
+			plot(out(:,1),out(:,2));
+		endfor
+	endfor
+	
+endfunction
