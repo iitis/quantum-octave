bf6830c285f73cd18fd262c421e847d09c097a42
jarekadam <jarekadam@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2007-04-19 12:01:30 +0000

    Spelling error

--- trunk/qo/IsState.m
+++ trunk/qo/IsState.m
@@ -32,33 +32,37 @@ why3 = "";
 if (nargin > 2 && nargin < 1)
 	usage ("IsState (state,[verb])");
 else
+	
 	# test if tr(mtx) = 1
 	if ( trace(mtx) == 1 )
 		norm = true;
 	else
-		why1 = "not normalize";
+		why1 = "not normalized";
 	end
+	
 	# test if mtx = mtx'
 	if ( mtx == mtx')
 		sadj = true;
 	else
 		why2 = "not selfadjoint";
 	end
+	
 	# test if matrix is ppositive
 	if ( min(eig(mtx)) < 0 )
 		why3 = "not positive definde";
 	else
 		posi = true;
 	end
-	end
+end
 	
-	if ( norm && sadj && posi )
-		ret = true;
- elseif	( nargin == 2 && verb == 1 )
-		printf("This matrix is not vaild state: is %s %s %s\n",why1,why2,why3);
-		ret =  false;
-	else
+if ( norm && sadj && posi )
+	ret = true;
+elseif ( nargin == 2 && verb == 1 )
+	printf("This matrix is not vaild state: is %s %s %s\n",why1,why2,why3);
+	ret = false;
+else
 	ret = false;
 end
+
 end
 end
