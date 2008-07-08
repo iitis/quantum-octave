7b8a163dc4636009f0630c38e5a6ef2c287f714e
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2008-07-08 10:53:49 +0000

    ---

--- qo/inst.tmp/IsMajorized.m
+++ qo/inst.tmp/IsMajorized.m
@@ -1,37 +1,40 @@
-%% -*- texinfo -*-
-%% @deftypefn {Function file} {} IsMajorized(v,w)
-%% Function @code{IsMajorized} checks if vector 
-%% @var{v} is majorized by %var{w}
-%% 
-%% @example 
-%% @group
-%% H()
-%%  @result{} 
-%%	0.70711   0.70711
-%%	0.70711  -0.70711
-%% @end group
-%% @end example
-%%
-%% @end deftypefn
-%%
-%% @seealso {IsMajorized2, PTrace}
-%%
+## -*- texinfo -*-
+## @deftypefn {Function file} {} ismajorizedvector(v,w)
+## Function @code{ismajorizedvector} checks if vector 
+## @var{v} is majorized by %var{w}
+## 
+## @example 
+## @group
+## ismajorizedvector(v,w)
+##  @result{} 
+##	0.70711   0.70711
+##	0.70711  -0.70711
+## @end group
+## @end example
+##
+## @end deftypefn
+##
+## @seealso {ismajorizedvector2, PTrace}
+##
 
-function ret = IsMajorized(v1, v2)
-	if (nargin ~= 2)
-		error ('Wrong number of arguments!');
+function ret = ismajorizedvector(v1, v2, precision)
+	if (nargin !=2 || nargin !=3)
+		usage('ismajorizedvector(v1, v2[, epsilon]));
 	elseif ( size(v1,2) ~= size(v2,2) )
-		error ('Arguments of different size!');
+		error ('Arguments of different size in ismajorizedvector()!');
 	else
+	if (nargin=2)
+		precision=10e-20
+	endif
 		s1 = sort(v1);
 		s2 = sort(v2);
 		vs = size(v1,2);
 		for i=vs:-1:1
-			if (sum(s1(i:vs)) >= sum(s2(i:vs)) + 10e-20 )
+			if (sum(s1(i:vs)) >= sum(s2(i:vs)) + precision)
 				ret = false;
 				return
-			end
-		end
+			endif
+		endif
 		ret = true;
-	end
-end
+	endif
+endfunction
