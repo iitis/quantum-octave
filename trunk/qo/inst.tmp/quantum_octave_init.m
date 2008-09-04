2654e223ab6d72b20c6c3df52ed514b7761f1708
piotr_gawron <piotr_gawron@99e43cdf-0e2a-0410-a593-d2dd67a16929>
2008-09-04 22:06:20 +0000

    minor changes

--- trunk/qo/inst.tmp/quantum_octave_init.m
+++ trunk/qo/inst.tmp/quantum_octave_init.m
@@ -2,5 +2,5 @@ function quantum_octave_init()
 	global quantum_octave_state;
 	quantum_octave_state=1;
 	global quantum_register_allocated;
-	quantum_register_allocated=[];
+	quantum_register_allocated={};
 endfunction
