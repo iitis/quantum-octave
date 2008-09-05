f58eae4b2a528c5264374d858e371c549652760c
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2008-09-05 11:04:37 +0000

    initial working implementation of register managament

--- qo/inst.tmp/newregister.m
+++ qo/inst.tmp/newregister.m
@@ -1,12 +1,12 @@
 function ret=newregister(regsize)
 	global quantum_octave_state;
-	quantum_octave_state=kron(quantum_octave_state, state(ketn(0,regsize)));
+	quantum_octave_state=__kron(quantum_octave_state, state(ketn(0,regsize)));
  	global quantum_register_allocated;
 	M=0;
-	for i=[1:length(quantum_register_allocated)]
-	
+	N=length(quantum_register_allocated);
+	for i=[1:N]
+		M=max(M,max(quantum_register_allocated{i}));
 	endfor
-	quantum_register_allocated={quantum_register_allocated };
-
-	ret=new;
+	quantum_register_allocated{N+1}=[M+1:M+regsize];
+ 	ret=N+1;
 endfunction
