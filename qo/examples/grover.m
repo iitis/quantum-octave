42045c04f5aeccf1881af80f7b29886109978d08
Piotr Gawron <718316+pgawron@users.noreply.github.com>
2008-09-09 23:16:37 +0000

    added noise to grover

--- qo/examples/grover.m
+++ qo/examples/grover.m
@@ -1,27 +1,31 @@
-function ret = grover( num, state )
+function ret = grover( num, statesize, channelname,p)
 # some data  
-	qubits = log2(size(state)(1));
+	st=state(ketn(0,statesize));
+	qubits = log2(size(st)(1));
 	tvec = [1:qubits];
-	k = ceil((pi/8)*sqrt(size(state)(1)));
-	bigh = ProductGate(qubits,H,tvec);
+	k = ceil((pi/8)*sqrt(size(st)(1)));
+	bigh = productgate(h,tvec,qubits);
 # Grover iterations
-	ret = Evolve(bigh,state);
+	ret = evolve(bigh,st);
 	for i = 1:k
-		ret = Evolve(oracle(num, qubits),ret);
-		ret = Evolve(diffuse(qubits),ret);
+		ret = evolve(oracle(num, qubits),ret);
+		ret = evolve(diffuse(qubits),ret);
+		if (nargin==4)
+			ret = applychannel(localchannel(channel(channelname,p),[1:qubits],qubits), ret);
+		endif
 	endfor
 endfunction
 
 function ret = oracle (num, qubits)
 # check if num<2^qubits
-	ret =  eye (2^qubits);
+	ret =  id (qubits);
 	ret(num+1,num+1) = -1;
 endfunction
 
 function ret = diffuse(qubits)
 	tvec = [1:qubits];
 	tmp = zeros(1,qubits);
-	ret = ProductGate(qubits,H,tvec);
-	ret = ret*(2*Projection(tmp) - Id(qubits));
-	ret = ret*ProductGate(qubits,H,tvec);
+	ret = productgate(h,tvec,qubits);
+	ret = ret*(2*projection(tmp) - id(qubits));
+	ret = ret*productgate(h,tvec,qubits);
 endfunction
